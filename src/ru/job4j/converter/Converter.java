17ffeca7faa041702358d350c525a89836b68209
Roman Semykin <KindPanic@mail.ru>
2021-04-14 20:33:55 +0300

    0. Что такое тестирование [#518082]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -10,6 +10,7 @@ public class Converter {
         int rsl = value / 60; /* формула перевода рублей в доллары. */
         return rsl;
     }
+
     public static int euroToRub(int value) {
         int rsl = value * 70; /* формула перевода евро в рубли. */
         return rsl;
@@ -21,7 +22,28 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        System.out.println("usd = 60 rubles");
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        in = 360;
+        expected = 6;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("360 rubles are 6. Test result : " + passed);
+        in = 3;
+        expected = 210;
+        out = Converter.euroToRub(in);
+        passed = expected == out;
+        System.out.println("3 euro are 210 rubles. Test result : " + passed);
+        in = 5;
+        expected = 300;
+        out = Converter.usdToRub(in);
+        passed = expected == out;
+        System.out.println("5 usd are 300 rubles. Test result : " + passed);
+
+        /*System.out.println("usd = 60 rubles");
         System.out.println("euro = 70 rubles");
         int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
@@ -31,5 +53,6 @@ public class Converter {
         int rubFromUsd = usdToRub(70);
         System.out.println("20 euro are " + rubFromEuro + " rubles.");
         System.out.println("70 dollars are " + rubFromUsd + " rubles.");
+*/
     }
 }
