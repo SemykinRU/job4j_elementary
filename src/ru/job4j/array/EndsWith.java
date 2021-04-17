4d88057dc006674ae379c9ea2322bf5a8bf22f41
Roman Semykin <KindPanic@mail.ru>
2021-04-17 22:43:49 +0300

    6.4.2. Слово заканчивается на ... [#518023]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        /* проверить. что массив word имеет последние элементы одинаковые с post */
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - 1] == post[post.length - 1]) {
+                result = true;
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
