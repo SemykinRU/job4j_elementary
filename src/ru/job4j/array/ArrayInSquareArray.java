49dadc81eadf417be25fc39f42c67da97419d702
Roman Semykin <KindPanic@mail.ru>
2021-04-20 23:13:01 +0300

    30. Превратить одномерный массив в двумерный.

--- src/ru/job4j/array/ArrayInSquareArray.java
+++ src/ru/job4j/array/ArrayInSquareArray.java
@@ -7,9 +7,11 @@ package ru.job4j.array;
 
 public class ArrayInSquareArray {
     public static int[][] convertArray(int[] array) {
-        int indexRow = 0;
-        int indexCell = 0;
-        int indexArray = array.length;
+
+        int indexRow = 0; //Счетчик строк в новом массиве.
+        int indexCell = 0; // Счетчик колонок в новом массиве.
+        int indexArray = array.length; //Счетчик для определения квадратной матрицы indexRow * indexCell == indexArray.
+        /*Выясняем, сколько нужно строк и колонок для нашей квадратной матрицы*/
         while (indexCell * indexRow != indexArray) {
             if (indexCell * indexRow < indexArray) {
                 indexCell++;
@@ -18,17 +20,21 @@ public class ArrayInSquareArray {
                 indexArray++;
             }
         }
-        int value = indexCell;
-        indexArray = indexCell;
+
+        int indexDip = indexCell; //Диапозон от i до indexDip - будем идти по основному массиву.
+        int count = indexCell; //Наш увеличитель диапозона, служит, что бы мы не вышли за рамки нашей строки в матрице.
+
       int[][] data = new int[indexRow][indexCell];
+      /*Сбрасываем наши идексы строк и колонок в 0*/
         indexCell = 0;
         indexRow = 0;
+
         for (int i = 0; i < array.length; i++) {
-            if (i < value) {
+            if (i < indexDip) {
                 data[indexRow][indexCell] = array[i];
                 indexCell++;
             } else {
-                value += indexArray;
+                indexDip += count; //Увеличиваем наш диапозон поиска на кол-во максимальных столбцов в строке.
                 indexCell = 0;
                 indexRow++;
                 i--;
