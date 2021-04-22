fd9edc85c720d34a2577b2b655903d38cc421257
Roman Semykin <KindPanic@mail.ru>
2021-04-22 15:11:08 +0300

    3. Объединить два отсортированных массива. Отредактированный.

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,39 +3,33 @@ package ru.job4j.array;
 /*3. Объединить два отсортированных массива*/
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Merge {
-
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int leftIndex = 0;
-        int rightIndex = 0;
-        int rslIndex = 0;
-      while (leftIndex < left.length && rslIndex < right.length) {
-          if (left[leftIndex] < right[rightIndex]) {
-              rsl[rslIndex] = left[leftIndex];
-              rslIndex++;
-              leftIndex++;
-          } else if (left[leftIndex] == right[rightIndex]) {
-              rsl[rslIndex] = left[leftIndex];
-              rsl[rslIndex + 1] = right[rslIndex];
-              leftIndex++;
-              rightIndex++;
-              rslIndex += 2;
-          } else {
-              rsl[rslIndex] = right[rightIndex];
-              rslIndex++;
-              rightIndex++;
-          }
-      }
-        for (int i = leftIndex; i < left.length; i++) {
-            rsl[rslIndex] = left[i];
-            rslIndex++;
-        }
-        for (int i = rightIndex; i < right.length; i++) {
-            rsl[rslIndex] = right[i];
-            rslIndex++;
+        int leftPoint = 0;
+        int rightPoint = 0;
+        int rslPoint = 0;
+
+        while (leftPoint + rightPoint != rsl.length) {
+            if (leftPoint != left.length && rightPoint != right.length) {
+                rsl[rslPoint++] = left[leftPoint] < right[rightPoint] ? left[leftPoint++] : right[rightPoint++];
+            } else if (leftPoint != left.length) {
+                rsl[rslPoint++] = left[leftPoint++];
+            } else {
+                rsl[rslPoint++] = right[rightPoint++];
+            }
         }
         return rsl;
+    }
+
+    public static void main(String[] args) {
+        Merge process = new Merge();
+        int[] rsl = process.merge(
+                new int[] {1, 3, 5},
+                new int[] {2, 4}
+        );
+        System.out.println(Arrays.toString(rsl));
     }
 }
