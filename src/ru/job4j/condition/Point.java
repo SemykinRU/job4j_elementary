937eafdbbdeda3714c2fe1f1a6e72e8e3582ae32
Roman Semykin <KindPanic@mail.ru>
2021-04-24 11:02:17 +0300

    2.1. Рефакторинг - Расстояние между точками. [#518214]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,27 +1,27 @@
 package ru.job4j.condition;
 
-public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-      /*  double rsl = Math.sqrt((Math.pow((x2-x1),2)+(Math.pow((y2-y1),2)))); */
-        double firstStep = x2 - x1;
-        double secondStep = y2 - y1;
-        double thirdStep = Math.pow(firstStep,2);
-        double fourthStep = Math.pow(secondStep,2);
-        double fifthStep = thirdStep + fourthStep;
-        double rsl = Math.sqrt(fifthStep);
+import static java.lang.Math.pow;
+import static java.lang.Math.sqrt;
 
-        return rsl;
+public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2,0 );
-        double result1 = Point.distance(1, 1, 1,1 );
-        double result2 = Point.distance(10, 6, 7,2 );
-        double result3 = Point.distance(1, 5, 3,3 );
-        System.out.println("result (0, 0) to (2, 0) = " + result);
-        System.out.println("result (1, 1) to (1, 1) = " + result1);
-        System.out.println("result (10, 6) to (7, 2) = " + result2);
-        System.out.println("result (1, 5) to (3, 3) = " + result3);
-
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
