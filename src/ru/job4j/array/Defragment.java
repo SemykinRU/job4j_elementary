2629c1353a55cf215c07236fcf8fe5e602f29bea
Roman Semykin <KindPanic@mail.ru>
2021-04-24 10:03:03 +0300

    6.8. Дефрагментация массива [#518045]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -6,9 +6,8 @@ public class Defragment {
             if (array[index] == null) {
                 for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        String str = array[i];
-                        array[i] = array[index];
-                        array[index] = str;
+                         array[index] = array[i];
+                         array[i] = null;
                         break;
                     }
                 }
