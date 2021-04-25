34bd6fa87a5677e71394c9900007109049ee164f
Roman Semykin <KindPanic@mail.ru>
2021-04-25 10:52:00 +0300

    3. Перегрузить метод max для трех чисел [#518217]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -31,4 +31,14 @@ public class MaxTest {
         int expected = 10;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenFirst1Second10Third5Then10() {
+        Assert.assertEquals(Max.max(1, 10, 5), 10);
+    }
+
+    @Test
+    public void whenFirst100Second0Third50Fourth0Then100() {
+        Assert.assertEquals(Max.max(100, 0, 50, 0), 100);
+    }
 }
